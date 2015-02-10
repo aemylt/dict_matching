0e222abeac162dc23a1fb948259b255f7124ae8a
Dominic Moylett <dm1905@bristol.ac.uk>
2015-02-10 14:58:59 +0000

    Switch to assertions for testing.

--- dict_matching.c
+++ dict_matching.c
@@ -1,9 +1,9 @@
+#include <assert.h>
 #include <string.h>
-#include <stdio.h>
 #include "dict_matching.h"
 
 char **P;
-int *m, num_patterns, n, alpha;
+int *m, num_patterns, n, alpha, *correct;
 
 void build_up() {
     int i;
@@ -11,15 +11,15 @@ void build_up() {
     for (i = 0; i < num_patterns; i++) {
         P[i] = malloc(sizeof(char) * (m[i] + 1));
     }
+    correct = malloc(sizeof(int) * n);
 }
 
 void stream_test(char *T) {
     dict_matcher matcher = dict_matching_build(P, m, num_patterns, n, alpha);
     int i;
     for (i = 0; i < n; i++) {
-        printf("%d,", dict_matching_stream(matcher, T[i], i));
+        assert(dict_matching_stream(matcher, T[i], i) == correct[i]);
     }
-    printf("\n");
     dict_matching_free(matcher);
 }
 
@@ -30,6 +30,7 @@ void tear_down() {
     }
     free(P);
     free(m);
+    free(correct);
 }
 
 void test_single_pattern() {
@@ -40,6 +41,11 @@ void test_single_pattern() {
     alpha = 0;
     build_up();
     strcpy(P[0], "ababbabb");
+    correct[0]  = -1; correct[1]  = -1; correct[2]  = -1; correct[3]  = -1; correct[4]  = -1; correct[5]  = -1;
+    correct[6]  = -1; correct[7]  = -1; correct[8]  = -1; correct[9]  = -1; correct[10] = -1; correct[11] = -1;
+    correct[12] = -1; correct[13] = -1; correct[14] = -1; correct[15] = -1; correct[16] = -1; correct[17] = -1;
+    correct[18] = -1; correct[19] = -1; correct[20] = -1; correct[21] = -1; correct[22] = -1; correct[23] = -1;
+    correct[24] = -1; correct[25] = -1; correct[26] = -1; correct[27] = -1; correct[28] = -1; correct[29] = 29;
     stream_test("ababbabcababbbbbababbaababbabb");
     tear_down();
 }
@@ -54,6 +60,11 @@ void test_two_patterns() {
     build_up();
     strcpy(P[0], "ababbabb");
     strcpy(P[1], "ababbabc");
+    correct[0]  = -1; correct[1]  = -1; correct[2]  = -1; correct[3]  = -1; correct[4]  = -1; correct[5]  = -1;
+    correct[6]  = -1; correct[7]  =  7; correct[8]  = -1; correct[9]  = -1; correct[10] = -1; correct[11] = -1;
+    correct[12] = -1; correct[13] = -1; correct[14] = -1; correct[15] = -1; correct[16] = -1; correct[17] = -1;
+    correct[18] = -1; correct[19] = -1; correct[20] = -1; correct[21] = -1; correct[22] = -1; correct[23] = -1;
+    correct[24] = -1; correct[25] = -1; correct[26] = -1; correct[27] = -1; correct[28] = -1; correct[29] = 29;
     stream_test("ababbabcababbbbbababbaababbabb");
     tear_down();
 }
