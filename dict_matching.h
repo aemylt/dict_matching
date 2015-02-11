a6ce635e6c7c0e92ccc71eb35ba091b2bcbbefb1
Dominic Moylett <dm1905@bristol.ac.uk>
2015-02-11 16:51:13 +0000

    Started work on breaking pattern up.
    Construction and destruction should be done.

--- dict_matching.h
+++ dict_matching.h
@@ -4,6 +4,36 @@
 #include <stdlib.h>
 #include "karp_rabin.h"
 #include "hash_lookup.h"
+#include "first_lookup.h"
+
+typedef struct {
+    int location;
+    fingerprint T_f;
+} viable_occurance;
+
+typedef struct {
+    int row_size, num_patterns, *start, *end;
+    hash_lookup lookup;
+    viable_occurance **VOs;
+} pattern_row;
+
+int shift_row(pattern_row row, fingerprint finger, int j) {
+    int i;
+    for (i = 0; i < row.num_patterns; i++) {
+        if ((row.start[i] != row.end[i]) && (row.VOs[i][row.start[i]].location == j)) {
+            fingerprint_assign(row.VOs[i][row.start[i]].T_f, finger);
+            if (++row.start[i] == row.row_size) row.start[i] = 0;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void add_occurance(pattern_row row, fingerprint finger, int location, int i) {
+    fingerprint_assign(finger, row.VOs[i][row.end[i]].T_f);
+    row.VOs[i][row.end[i]].location = i + row.row_size;
+    if (++row.end[i] == row.row_size) row.end[i] = 0;
+}
 
 typedef struct dict_matcher_t {
     fingerprinter printer;
@@ -14,6 +44,10 @@ typedef struct dict_matcher_t {
     char *text;
     int m;
     hash_lookup lookup;
+
+    pattern_row *rows;
+    int num_rows;
+    first_lookup first_round;
 } *dict_matcher;
 
 dict_matcher dict_matching_build(char **P, int *m, int num_patterns, int n, int alpha) {
@@ -33,10 +67,60 @@ dict_matcher dict_matching_build(char **P, int *m, int num_patterns, int n, int 
     matcher->T_j = init_fingerprint();
     matcher->tmp = init_fingerprint();
     matcher->lookup = hashlookup_build(patterns, num_patterns, matcher->printer);
+
+    int m_max = m[0];
+    matcher->num_rows = 0;
+    while ((1 << matcher->num_rows) < m_max) {
+        matcher->num_rows++;
+    }
+    matcher->rows = malloc(sizeof(pattern_row) * matcher->num_rows);
+
+    int j, k, lookup_size;
+    char *first_letters = malloc(sizeof(char) * num_patterns);
+    lookup_size = 0;
+    for (j = 0; j < num_patterns; j++) {
+        first_letters[lookup_size] = P[j][0];
+        for (k = 0; k < lookup_size; k++) {
+            if (first_letters[k] == first_letters[lookup_size]) break;
+        }
+        if (k == lookup_size) lookup_size++;
+    }
+    matcher->first_round = firstlookup_build(first_letters, lookup_size);
+    free(first_letters);
+
+    i = 1;
+    lookup_size = 0;
+    while ((1 << i) <= m_max) {
+        matcher->rows[i - 1].row_size = 1 << i;
+        lookup_size = 0;
+        for (j = 0; j < num_patterns; j++) {
+            set_fingerprint(matcher->printer, P[j], matcher->rows[i - 1].row_size << 1, patterns[lookup_size]);
+            for (k = 0; k < lookup_size; k++) {
+                if (fingerprint_equals(patterns[k], patterns[lookup_size])) break;
+            }
+            if (k == lookup_size) lookup_size++;
+        }
+        matcher->rows[i - 1].lookup = hashlookup_build(patterns, lookup_size, matcher->printer);
+        matcher->rows[i - 1].num_patterns = lookup_size;
+        matcher->rows[i - 1].start = malloc(sizeof(int) * lookup_size);
+        matcher->rows[i - 1].end = malloc(sizeof(int) * lookup_size);
+        matcher->rows[i - 1].VOs = malloc(sizeof(viable_occurance*) * lookup_size);
+        for (j = 0; j < lookup_size; j++) {
+            matcher->rows[i - 1].start[j] = 0;
+            matcher->rows[i - 1].end[j] = 0;
+            matcher->rows[i - 1].VOs[j] = malloc(sizeof(viable_occurance) * matcher->rows[i - 1].row_size);
+            for (k = 0; k < matcher->rows[i - 1].row_size; k++) {
+                matcher->rows[i - 1].VOs[j][k].T_f = init_fingerprint();
+            }
+        }
+        i++;
+    }
+
     for (i = 0; i < num_patterns; i++) {
         fingerprint_free(patterns[i]);
     }
     free(patterns);
+
     return matcher;
 }
 
@@ -63,6 +147,20 @@ void dict_matching_free(dict_matcher matcher) {
     fingerprinter_free(matcher->printer);
     free(matcher->text);
     hashlookup_free(&matcher->lookup);
+
+    firstlookup_free(&matcher->first_round);
+    int i, j, k;
+    for (i = 0; i < matcher->num_rows; i++) {
+        for (j = 0; j < matcher->rows[i].num_patterns; j++) {
+            for (k = 0; k < matcher->rows[i].row_size; k++) {
+                fingerprint_free(matcher->rows[i].VOs[j][k].T_f);
+            }
+            free(matcher->rows[i].VOs[j]);
+        }
+        hashlookup_free(&matcher->rows[i].lookup);
+    }
+    free(matcher->rows);
+
     free(matcher);
 }
 
---
+++ first_lookup.h
@@ -0,0 +1,61 @@
+#ifndef __FIRST_LOOKUP__
+#define __FIRST_LOOKUP__
+
+typedef struct {
+    cmph_t *hash;
+    int num;
+    char *keys;
+} first_lookup;
+
+first_lookup firstlookup_build(char *key_string, int num) {
+    first_lookup lookup;
+    lookup.num = num;
+
+    if (num > 1) {
+        char **keys = malloc(sizeof(char*) * num);
+        int i;
+        for (i = 0; i < num; i++) {
+            keys[i] = malloc(sizeof(char) * 2);
+            keys[i][0] = key_string[i];
+            keys[i][1] = '\0';
+        }
+        cmph_io_adapter_t *source = cmph_io_vector_adapter(keys, num);
+        cmph_config_t *config = cmph_config_new(source);
+        cmph_config_set_algo(config, CMPH_CHD);
+        lookup.hash = cmph_new(config);
+        cmph_config_destroy(config);
+        cmph_io_vector_adapter_destroy(source);
+        lookup.keys = malloc(num * sizeof(char));
+
+        unsigned int id;
+        char *key;
+        for (i = 0; i < num; i++) {
+            key = keys[i];
+            id = cmph_search(lookup.hash, key, 1);
+            lookup.keys[id] = key_string[i];
+            free(keys[i]);
+        }
+        free(keys);
+    } else if (num == 1) {
+        lookup.keys = malloc(sizeof(char));
+        lookup.keys[0] = key_string[0];
+    }
+
+    return lookup;
+}
+
+int firstlookup_search(first_lookup lookup, char key) {
+    if (lookup.num == 0) return -1;
+    if (lookup.num == 1) return (key == lookup.keys[0]) ? 0 : -1;
+    int id = cmph_search(lookup.hash, &key, 1);
+    return ((id < lookup.num) && (key == lookup.keys[id])) ? id : -1;
+}
+
+void firstlookup_free(first_lookup *lookup) {
+    if (lookup->num > 0) {
+        if (lookup->num > 1) cmph_destroy(lookup->hash);
+        free(lookup->keys);
+    }
+}
+
+#endif
