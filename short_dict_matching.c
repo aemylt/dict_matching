f5d288e0d157436d64f1bc75da9bc3423959f37f
Dominic Moylett <dm1905@bristol.ac.uk>
2015-03-03 14:16:49 +0000

    Readded test for patterns of k length.

--- short_dict_matching.c
+++ short_dict_matching.c
@@ -42,6 +42,28 @@ void tear_down() {
     free(correct);
 }
 
+void test_four_patterns() {
+    n = 46;
+    num_patterns = 4;
+    m = malloc(sizeof(int) * num_patterns);
+    m[0] = 4; m[1] = 4; m[2] = 4; m[3] = 2;
+    build_up();
+    strcpy(P[0], "take");
+    strcpy(P[1], "fast");
+    strcpy(P[2], "sofa");
+    strcpy(P[3], "so");
+    correct[0]  = -1; correct[1]  = -1; correct[2]  = -1; correct[3]  = 3; correct[4]  = -1; correct[5]  = 5;
+    correct[6]  = -1; correct[7]  = -1; correct[8]  = -1; correct[9]  = -1; correct[10] = 10; correct[11] = -1;
+    correct[12] = 12; correct[13] = -1; correct[14] = 14; correct[15] = -1; correct[16] = -1; correct[17] = -1;
+    correct[18] = -1; correct[19] = -1; correct[20] = 20; correct[21] = -1; correct[22] = 22; correct[23] = -1;
+    correct[24] = -1; correct[25] = -1; correct[26] = 26; correct[27] = -1; correct[28] = -1; correct[29] = 29;
+    correct[30] = -1; correct[31] = -1; correct[32] = 32; correct[33] = -1; correct[34] = -1; correct[35] = 35;
+    correct[36] = -1; correct[37] = 37; correct[38] = -1; correct[39] = 39; correct[40] = -1; correct[41] = -1;
+    correct[42] = 42; correct[43] = -1; correct[44] = -1; correct[45] = 45;
+    stream_test("takeso fasofast fassofatake sosso sofastake so", n, P, m, num_patterns, correct);
+    tear_down();
+}
+
 void test_seven_patterns() {
     n = 6;
     num_patterns = 7;
@@ -61,6 +83,7 @@ void test_seven_patterns() {
 }
 
 int main(void) {
+    test_four_patterns();
     test_seven_patterns();
     printf("All tests succeeded!\n");
     return 0;
